0e572102d78357df043056e568bb43a88e990913
hafsana-bgm <hafsas.ht@gmail.com>
2025-01-14 14:41:14 +0600

    Initial commit

---
+++ Areas/C_Area/Data/C_AreaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace C_Area.Areas.C_Area.Data
+{
+    public class C_AreaDbContext : IdentityDbContext
+    {
+        public C_AreaDbContext(DbContextOptions<C_AreaDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
